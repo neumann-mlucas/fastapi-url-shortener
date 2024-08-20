7a90bda071805c797dc2e51bcb0fd5e5cea2d4b0
neumann-mlucas <neumann.m.lucas@gmail.com>
2024-08-20 00:51:55 -0300

    update some docstrings

--- app/controller/url_controller.py
+++ app/controller/url_controller.py
@@ -29,61 +29,71 @@ router = APIRouter(prefix="/api/v1", tags=["UrlShortener API"])
 
 @router.get("/{hash}", response_model=UrlResponse)
 async def get(hash: str, db: AsyncSession = Depends(get_db)):
-    "given a valid hash, returns the corresponding URL if it's active"
+    """Retrieve a URL by its hash if it's active, using cache if available."""
     if not valid_hash(hash):
-        raise HTTPException(status_code=400, detail="invalid short url")
+        raise HTTPException(status_code=400, detail="Invalid short URL")
 
     rd = await get_redis()
-    if cached := await rd.get(hash):  # get cached short url
+
+    # check if short URL alreadt in cache
+    if cached := await rd.get(hash):
         UrlResponse(data=UrlModel(url=cached, hash=hash, on=True))
 
+    # query the SQL DB
     record = await url_repository.get(hash, db)
-    if record is None:
-        raise HTTPException(status_code=404, detail="url not found")
-    if not record.on:  # only return active short URL
-        raise HTTPException(status_code=404, detail="url not found")
+    if record is None or not record.on:
+        raise HTTPException(status_code=404, detail="URL not found")
 
-    await rd.set(record.hash, str(record.url))  # cache short url
+    # cache short URL
+    await rd.set(record.hash, str(record.url))
+
     return UrlResponse(data=record)
 
 
 @router.get("/", response_model=MultipleUrlsResponse)
 async def get_all(db: AsyncSession = Depends(get_db)):
-    "debugging route, returns all short URLs"
+    """Retrieve all URL records."""
     records = await url_repository.get_all(db)
     if records is None or not len(records):
-        raise HTTPException(status_code=404, detail="no register found in db")
+        raise HTTPException(status_code=404, detail="No URLs found")
     return MultipleUrlsResponse(data=records)
 
 
 @router.post("/", response_model=UrlResponse)
 async def create(data: UrlRequest, db: AsyncSession = Depends(get_db)):
-    "register a new short URL. if URL is already register, returns the old link"
+    """Register a new short URL, or return the existing one if already registered."""
+    rd = await get_redis()
+
+    # try to add URL to database
     record = await url_repository.add(str(data.url), db)
     if record:
+        await rd.set(record.hash, str(record.url))  # cache short URL
         return UrlResponse(data=record)
 
-    # case where URL already in database
-    record = await url_repository.get_by_url(str(data.url), db)
-    if record is None:  # race condition with delete?
-        raise HTTPException(status_code=404, detail="record was deleted, try again")
+    # case where the URL is already in the DB
+    existing_record = await url_repository.get_by_url(str(data.url), db)
+    if existing_record is None:  # probalby not needed
+        raise HTTPException(
+            status_code=404, detail="Record was deleted or not found, try again"
+        )
 
-    # cache the new url
-    rd = await get_redis()
-    await rd.set(record.hash, str(record.url))
+    # cache the old short url just to be sure
+    await rd.set(existing_record.hash, str(existing_record.url))
 
-    return UrlResponse(data=record, status="warning", errors="url already in db")
+    return UrlResponse(
+        data=existing_record, status="warning", errors="URL already in database"
+    )
 
 
 @router.delete("/{hash}", response_model=UrlResponse)
 async def delete(hash: str, db: AsyncSession = Depends(get_db)):
-    "dev route, deletes a short URL from the database"
+    """Delete a URL record by its hash."""
     if not valid_hash(hash):
-        raise HTTPException(status_code=400, detail="invalid short url")
+        raise HTTPException(status_code=400, detail="Invalid short URL")
 
     record = await url_repository.delete(hash, db)
     if record is None:
-        raise HTTPException(status_code=404, detail="no register found in db")
+        raise HTTPException(status_code=404, detail="No record found to delete")
 
     # sync cache with db
     rd = await get_redis()
@@ -94,14 +104,14 @@ async def delete(hash: str, db: AsyncSession = Depends(get_db)):
 
 @router.put("/{hash}", response_model=UrlResponse)
 async def update(hash: str, data: UrlRequest, db: AsyncSession = Depends(get_db)):
-    "dev route, update the original URL of a given short URL"
+    """Update the URL of a given short URL."""
     if not valid_hash(hash):
-        raise HTTPException(status_code=400, detail="invalid short url")
+        raise HTTPException(status_code=400, detail="Invalid short URL")
 
     input = UrlModel(hash=hash, url=str(data.url), on=None)
     record = await url_repository.update(input, db)
     if record is None:
-        raise HTTPException(status_code=404, detail="no register found in db")
+        raise HTTPException(status_code=404, detail="No record found to update")
 
     # sync cache with db
     rd = await get_redis()
@@ -112,25 +122,26 @@ async def update(hash: str, data: UrlRequest, db: AsyncSession = Depends(get_db)
 
 @router.put("/activate/{hash}", response_model=UrlResponse)
 async def activate(hash: str, db: AsyncSession = Depends(get_db)):
-    "dev route, activates a short URL"
+    """Activate a short URL."""
     if not valid_hash(hash):
-        raise HTTPException(status_code=400, detail="invalid short url")
+        raise HTTPException(status_code=400, detail="Invalid short URL")
 
     record = await url_repository.update(UrlModel(hash=hash, url=None, on=True), db)
     if record is None:
-        raise HTTPException(status_code=404, detail="no register found in db")
+        raise HTTPException(status_code=404, detail="No record found to activate")
+
     return UrlResponse(data=record)
 
 
 @router.put("/deactivate/{hash}", response_model=UrlResponse)
 async def deactivate(hash: str, db: AsyncSession = Depends(get_db)):
-    "dev route, deactivates a short URL"
+    """Deactivate a short URL."""
     if not valid_hash(hash):
-        raise HTTPException(status_code=400, detail="invalid short url")
+        raise HTTPException(status_code=400, detail="Invalid short URL")
 
     record = await url_repository.update(UrlModel(hash=hash, url=None, on=False), db)
     if record is None:
-        raise HTTPException(status_code=404, detail="no register found in db")
+        raise HTTPException(status_code=404, detail="No record found to deactivate")
 
     # sync cache with db
     rd = await get_redis()
